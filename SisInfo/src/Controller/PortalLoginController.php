d23ad6c5e75a4c2f02c8179e13ced9dbf78e71a5
windsonPatricio <54744102+windsonPatricio@users.noreply.github.com>
2020-12-13 17:17:27 -0300

    #12 estruturacao
    padrao MVC

---
+++ SisInfo/src/Controller/PortalLoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ifnc\Tads\Controller;
+
+
+use Ifnc\Tads\Entity\Pacote;
+use Ifnc\Tads\Helper\Render;
+use Ifnc\Tads\Helper\Transaction;
+
+class PortalLoginController implements IController
+{
+
+    public function request(): void
+    {
+        Transaction::open();
+        echo Render::html(
+            [
+                "index.php"
+            ],[
+
+            "titulo"=>"Sisinfo"
+
+        ]);
+    }
+}
