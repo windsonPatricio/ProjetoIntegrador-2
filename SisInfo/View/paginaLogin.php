87939aa8856a1880fabffea8f6ba4ea11da151e2
wilson Patricio <wilson.patricio.70@gmail.com>
2020-12-14 20:57:52 -0300

    #9 acrescentando
    adapantando componentes para teste posteriores e arquivos de ajustes.

--- SisInfo/View/paginaLogin.php
+++ SisInfo/View/paginaLogin.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Projeto Intergrador 2Sem</title>
+	<meta name="google-signin-client_id" content="704885881523-70al1favqv1gcbqemtuc27lslb6trsc9.apps.googleusercontent.com">
+    <title>SisInfo</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/Article-List.css">
@@ -12,6 +13,7 @@
     <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
     <link rel="stylesheet" href="assets/css/Newsletter-Subscription-Form.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
 </head>
 
 <body style="color: #fbf8fc;background-color: #ffff;height: 650px;">
@@ -20,6 +22,7 @@
             <h2 class="sr-only">Login Form</h2>
             <div class="illustration" style="background-image: url(&quot;assets/img/SIS%20INFO.jpg&quot;);height: 214px;margin: 0px;padding: 0px;color: rgb(244,240,241);background-size: cover;"></div>
             <h2 class="text-center" style="color: rgb(7,9,10);font-size: 18px;padding: 2px;height: 28px;width: 236px;">Entre com sua conta Google</h2>
+			<div id="meu-botao"></div>
         </form>
     </div>
     <form method="post" style="background-color: #ffff;">
@@ -27,6 +30,9 @@
     </form>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+	<script src="assets/js/formas.js"></script>
 </body>
 
 </html>
+
