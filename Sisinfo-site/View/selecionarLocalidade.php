216c4898b0e7eb64739bf9b5bc0e4cb994c56c20
windsonPatricio <54744102+windsonPatricio@users.noreply.github.com>
2020-12-19 23:22:34 -0300

    #10 configracao de localidade
    metodos para selecao do localizacao (UF, cidade);

--- Sisinfo-site/View/selecionarLocalidade.php
+++ Sisinfo-site/View/selecionarLocalidade.php
@@ -19,27 +19,25 @@
 <body class="text-center" style="background-image: url(&quot;assets/img/13162448904561.jpg&quot;);background-size: cover;background-repeat: no-repeat;">
     <h2 class="text-center" style="color: rgb(7,9,10);font-size: 24px;padding: 0;height: 78px;margin: 15px;letter-spacing: 0px;font-style: normal;font-weight: bold;">Para onde vamos?</h2>
 
-    <form method="post">
-        <select class="border rounded-0 border-dark shadow-lg" style="height: 50px;width: 121px;margin: 28px;font-size: 20px;">
-            <optgroup label="Estado">
-                <option value="12" selected="">Selecione </option>
-                <option value="13">This is item 2</option>
-                <option value="14">This is item 3</option>
-                </optgroup>
-        </select>
-        <select class="border rounded-0 border-dark shadow-lg" style="height: 50px;width: 200px;font-size: 20px;">
-            <optgroup label="Cidade">
-                <option value="12" selected="">Selecione</option>
-                <option value="13">This is item 2</option>
-                <option value="14">This is item 3</option>
-            </optgroup>
-        </select>
+    <form action="" id="create-class">
+    <fieldset>
 
+                <select name="uf" required class="border rounded-0 border-dark shadow-lg" style="height: 50px;width: 160px;margin: 28px;font-size: 20px;">
+                    <option value="">Selecione o Estado</option>
+                </select>
+                <input type="hidden" name="state">
+
+                <label for="city"></label>
+                <select name="city" disabled required class="border rounded-0 border-dark shadow-lg" style="height: 50px;width: 200px;font-size: 20px;">
+                    <option value="">Selecione a Cidade</option>
+                </select>
+
+    </fieldset>
         <a class="btn btn-success" href="\pagInicial">Selecionar</a>
+
     </form>
-
-
     <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/estado-municipio.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
